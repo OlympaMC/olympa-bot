a1c46777dcc279b9780f1e194f66146aa0c18927
Tristiisch <tristiisch74@hotmail.com>
2020-06-28 21:17:48 +0200

    fix

--- src/java/fr/olympa/bot/discord/sql/CacheDiscordSQL.java
+++ src/java/fr/olympa/bot/discord/sql/CacheDiscordSQL.java
@@ -15,14 +15,14 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 
 public class CacheDiscordSQL {
-	
+
 	// <discordId, DiscordMember>
 	private static Cache<Long, DiscordMember> cacheMembers = CacheBuilder.newBuilder().expireAfterAccess(1, TimeUnit.HOURS).build();
-
+	
 	public static DiscordMember getDiscordMember(User user) throws SQLException {
 		return getDiscordMember(user.getIdLong());
 	}
-
+	
 	public static DiscordMember getDiscordMember(long userDiscordId) throws SQLException {
 		DiscordMember discordMember = cacheMembers.asMap().get(userDiscordId);
 		if (discordMember == null) {
@@ -32,7 +32,7 @@ public class CacheDiscordSQL {
 		}
 		return discordMember;
 	}
-	
+
 	public static DiscordMember getDiscordMemberByOlympaId(long olympaId) throws SQLException {
 		DiscordMember discordMember = cacheMembers.asMap().values().stream().filter(dm -> dm.getOlympaId() == olympaId).findFirst().orElse(null);
 		if (discordMember == null) {
@@ -42,24 +42,24 @@ public class CacheDiscordSQL {
 		}
 		return discordMember;
 	}
-
+	
 	private static void setDiscordMember(long userDiscordId, DiscordMember discordMember) {
 		cacheMembers.put(userDiscordId, discordMember);
 	}
-
+	
 	// <discordId, DiscordMember>
 	public static Cache<Long, DiscordMessage> cacheMessage = CacheBuilder.newBuilder().expireAfterAccess(1, TimeUnit.HOURS).build();
-
+	
 	public static Entry<Long, DiscordMessage> getDiscordMessage(OlympaGuild olympaGuild, Message message) throws SQLException {
 		return getDiscordMessage(olympaGuild.getId(), message.getTextChannel().getIdLong(), message.getIdLong());
 	}
-	
+
 	public static Entry<Long, DiscordMessage> getDiscordMessage(long olympaGuildId, long channelDiscordId, long messageDiscordId) throws SQLException {
 		Entry<Long, DiscordMessage> entry = cacheMessage.asMap().entrySet().stream().filter(e -> e.getValue().getMessageId() == messageDiscordId).findFirst().orElse(null);
 		if (entry == null) {
 			DiscordMessage discordMessage = DiscordSQL.selectMessage(olympaGuildId, channelDiscordId, messageDiscordId);
 			if (discordMessage != null) {
-				DiscordMember discordMember = getDiscordMember(discordMessage.getOlympaDiscordAuthorId());
+				DiscordMember discordMember = getDiscordMemberByOlympaId(discordMessage.getOlympaDiscordAuthorId());
 				entry = new AbstractMap.SimpleEntry<>(discordMember.getDiscordId(), discordMessage);
 				if (entry != null)
 					cacheMessage.put(entry.getKey(), entry.getValue());
@@ -67,7 +67,7 @@ public class CacheDiscordSQL {
 		}
 		return entry;
 	}
-
+	
 	public static void setDiscordMessage(long userDiscordId, DiscordMessage discordMessage) {
 		cacheMessage.put(userDiscordId, discordMessage);
 	}
