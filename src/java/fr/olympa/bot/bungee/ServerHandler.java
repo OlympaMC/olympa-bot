2052caae160e67075a3f15cd97d39c4bf563e187
Tristiisch <tristiisch74@hotmail.com>
2020-07-02 06:10:32 +0200

    up

--- src/java/fr/olympa/bot/bungee/ServerHandler.java
+++ src/java/fr/olympa/bot/bungee/ServerHandler.java
@@ -1,9 +1,7 @@
 package fr.olympa.bot.bungee;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.PatternSyntaxException;
 
 import fr.olympa.core.bungee.OlympaBungee;
 import net.dv8tion.jda.api.entities.Member;
@@ -24,13 +22,35 @@ public class ServerHandler {
 				StringBuilder sb = new StringBuilder();
 				while ((s = br.readLine()) != null)
 					sb.append(s);
-				String out = sb.toString().replaceAll("(\\[\\]|)", "").replace("[0m", "").replace("[32m", "**").replace("[36m", "**").replace("[36m", "**").replace("[0;36m", "**").replace("[31m", "**").replace("[0;31m", "**");
+				String out = sb.toString().replace("0;", "").replace("", "")
+						.replace("[0m", "§f")
+						.replace("[1m", "§l")
+						.replace("[4m", "§n")
+						.replace("[32m", "§3")
+						.replace("[36m", "§b")
+						.replace("[49m", "§f")
+						.replace("[30m", "§0")
+						.replace("[31m", "§4")
+						.replace("[32m", "§2")
+						.replace("[33m", "§6")
+						.replace("[34m", "§1")
+						.replace("[35m", "§5")
+						.replace("[36m", "§3")
+						.replace("[37m", "§7")
+						.replace("[90m", "§8")
+						.replace("[91m", "§c")
+						.replace("[92m", "§a")
+						.replace("[91m", "§e")
+						.replace("[91m", "§9")
+						.replace("[91m", "§d")
+						.replace("[91m", "§b")
+						.replace("[97m", "§f");
 				channel.sendMessage(member.getAsMention() + " " + out).queue();
 				OlympaBungee.getInstance().sendMessage("§c[§4OUT§c] §c" + out);
 				p.waitFor();
 				p.destroy();
-			} catch (IOException | InterruptedException | PatternSyntaxException e) {
-				channel.sendMessage(member.getAsMention() + " " + e.getMessage()).queue();
+			} catch (Exception e) {
+				channel.sendMessage(member.getAsMention() + " **ERREUR**: " + e.getMessage()).queue();
 				e.printStackTrace();
 			}
 		}).start();
