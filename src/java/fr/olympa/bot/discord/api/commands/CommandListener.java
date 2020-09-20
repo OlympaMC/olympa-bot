911e8a157648af07a7f5c2aa060818da4926d993
Tristiisch <tristiisch74@hotmail.com>
2020-09-21 00:42:10 +0200

    fix

--- src/java/fr/olympa/bot/discord/api/commands/CommandListener.java
+++ src/java/fr/olympa/bot/discord/api/commands/CommandListener.java
@@ -58,7 +58,7 @@ public class CommandListener extends ListenerAdapter {
 		if (label.isEmpty())
 			return;
 		DiscordCommand discordCommand = DiscordCommand.getCommand(label);
-		if (discordCommand.checkPrivateChannel(message, user)) {
+		if (discordCommand == null) {
 			channel.sendMessage("Désolé " + user.getAsMention() + " mais cette commande n'existe pas. Fait .help pour voir la liste des commandes.").queue();
 			return;
 		}
