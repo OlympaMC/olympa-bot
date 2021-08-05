481e7a0ea69834cfa6981ec4d0c12bb29cf1fcfb
Tristiisch <tristiisch74@hotmail.com>
2021-08-05 03:23:27 +0200

    fix compile x3 (décidément)

--- src/java/fr/olympa/bot/discord/message/DiscordMessage.java
+++ src/java/fr/olympa/bot/discord/message/DiscordMessage.java
@@ -14,6 +14,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
+import fr.olympa.api.LinkSpigotBungee;
 import fr.olympa.bot.OlympaBots;
 import fr.olympa.bot.discord.api.DiscordUtils;
 import fr.olympa.bot.discord.guild.GuildHandler;
@@ -63,14 +64,14 @@ public class DiscordMessage {
 	}
 
 	public DiscordMessage(long olympaGuildId, long channelId, long messageId, long olympaDiscordAuthorId, String contents, Timestamp created, long logMessageId) {
-		olympaGuildId = olympaGuildId;
-		channelId = channelId;
-		messageId = messageId;
-		olympaDiscordAuthorId = olympaDiscordAuthorId;
+		this.olympaGuildId = olympaGuildId;
+		this.channelId = channelId;
+		this.messageId = messageId;
+		this.olympaDiscordAuthorId = olympaDiscordAuthorId;
 		if (contents != null && !contents.isBlank())
-			contents = new Gson().fromJson(contents, new TypeToken<List<MessageContent>>() {}.getType());
-		created = created.getTime() / 1000L;
-		logMessageId = logMessageId;
+			this.contents = new Gson().fromJson(contents, new TypeToken<List<MessageContent>>() {}.getType());
+		this.created = created.getTime() / 1000L;
+		this.logMessageId = logMessageId;
 	}
 
 	public long getGuildId() {
